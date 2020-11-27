cc496e2038ed72af1af420c92effafa7e3c73355
pparkst <pparkst@naver.com>
2020-11-27 22:25:54 +0900

    Initial Commit

---
+++ SwiftUIMemo/MemoListScene.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemo
+//
+//  Created by pparkst on 2020/11/27.
+//  Copyright © 2020 pparkst. All rights reserved.
+//
+
+import SwiftUI
+
+struct MemoListScene: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MemoListScene_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListScene()
+    }
+}
